bc497d613054158aa1a46caf3ad149f5effbd4ba
Tony Castronova <castronova.anthony@gmail.com>
2014-03-17 13:31:54 -0600

    Basic HydroModel resource is working.

--- models.py
+++ models.py
@@ -9,5 +9,14 @@ from hs_core.models import AbstractResource
 # To create a new resource, use these three super-classes. 
 #
 
-# class MyResource(Page, RichText, AbstractResource)
+class HydromodelResource(Page, RichText, AbstractResource):
+
+    resource_description =  models.TextField(null=False,blank=True,default='My Text Field')
+    resource_file = models.FileField(verbose_name='Model Archive',name='model_archive',upload_to='hs.hydromodel',storage=None,
+                                     help_text='Upload Model Archive as *.ZIP')
+
+
+    class Meta:
+        verbose_name = 'HydroModel'
+
 
