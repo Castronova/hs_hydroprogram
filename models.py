62b5f839dc74dc7deb8bc27bb7d2bad3f426976e
Tony Castronova <castronova.anthony@gmail.com>
2014-06-24 15:50:35 -0600

    Implemented the hydroprogram metadata model presented at AWRA

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ from hs_core.models import AbstractResource
 # To create a new resource, use these three super-classes. 
 #
 
-class HydromodelResource(Page, RichText, AbstractResource):
+class HydroProgramResource(Page, RichText, AbstractResource):
 
     # This will save the file in ./static/media/hs/hydromodel
     hm_zip = models.FileField(verbose_name='Model Archive',name='modelzip',upload_to='./hs/hydromodel',
@@ -27,16 +27,134 @@ class HydromodelResource(Page, RichText, AbstractResource):
     # if instance, choose parent model or create parent model
 
 
-    #######################
-    # TEMPORAL DEFINITION #
-    #######################
-    # Only for Instance Types
-    hm_begin = models.DateTimeField(verbose_name='Simulation Begin',
-                                 help_text='The start date of the model simulation (mm/dd/yyyy hh:mm)')
-    hm_end  = models.DateTimeField(verbose_name='Simulation End',
-                                help_text='The end date of the model simulation (mm/dd/yyyy hh:mm)')
-    hm_timestep = models.FloatField(verbose_name='Simulation Time Interval',
-                                 help_text='The timestep interval that is used for calculations (in seconds)')
+    ###########
+    # Program #
+    #---------#
+    # 1 .. 1  #
+    ###########
+
+    # title
+    title = models.TextField(verbose_name='Program Title', null=False, blank=True, default='',
+                             help_text='The title of the program')
+
+    # organization
+    organization = models.TextField(verbose_name='Organization Name', null=False, default='',
+                                    help_text='The organization affiliated with this program')
+
+    # description
+    description = models.TextField(verbose_name='Program Description', null=False, default='',
+                                   help_text="A brief description of the program")
+
+    # subject
+    subject = models.TextField(verbose_name='Subject Keywords', null=False, default='',
+                               help_text='Subject keywords describing the program, delimited by semicolon')
+
+    # format
+    format =  'application/octet-stream'
+
+    # type
+    type = 'HydroProgram'
+
+    # url
+    program_url = models.TextField(verbose_name='Program URL', null=True, default=None,
+                           help_text='A URL providing additional information about the program as a whole (e.g. website, documentation, etc...)')
+
+    ############
+    # SOFTWARE #
+    #----------#
+    #  1 .. 1  #
+    ############
+
+    # url
+    software_url = models.TextField(verbose_name='Software URL', null=True, default= None,
+                                    help_text='A URL providing addition information about the software (e.g. source repository, source download, etc...')
+
+    # date_released
+    date_released = models.DateTimeField(verbose_name='Date of Software Release',
+                                        help_text='The date of the software release (mm/dd/yyyy hh:mm)')
+
+    # version
+    software_version = models.TextField(verbose_name="Software Version", null=False,default='',
+                                        help_text="The version of the software release")
+
+    # rights
+    software_rights = models.TextField(verbose_name="Software Rights", null=False,default="",
+                                       help_text="The software rights of the program (e.g. http://creativecommons.org/licenses/by/4.0)")
+
+    ####################
+    #    REQUIREMENT   #
+    #------------------#
+    # cardinality 0..* #
+    ####################
+
+    # subject
+    req_subject = models.TextField(verbose_name='Subject Keywords', null=True, default=None,
+                               help_text='Subject keywords describing the software requirement, delimited by semicolon')
+
+    # description
+    req_description = models.TextField(verbose_name="Requirement Description", null=False,default='',
+                                       help_text="A description of the software requirement")
+
+    # url
+    req_url = models.TextField(verbose_name='Requirement URL', null=True, default=None,
+                               help_text="A URL providing additional information regarding the software requirement.")
+
+
+    ###########
+    # CREATOR #
+    #---------#
+    #  1 .. 1 #
+    ###########
+
+    # name
+    # organization
+    # address
+    # email
+    # phone
+    # url
+
+    ###############
+    # CONTRIBUTOR #
+    #-------------#
+    #    0 .. *   #
+    ###############
+
+    # name
+    # organization
+    # address
+    # email
+    # phone
+    # url
+
+    ########
+    # Date #
+    #------#
+    # 1..1 #
+    ########
+
+    # created
+    # modified
+
+
+
+
+
+
+
+
+
+
+    # #######################
+    # # TEMPORAL DEFINITION #
+    # #######################
+    # # Only for Instance Types
+    # hm_begin = models.DateTimeField(verbose_name='Simulation Begin',
+    #                              help_text='The start date of the model simulation (mm/dd/yyyy hh:mm)')
+    # hm_end  = models.DateTimeField(verbose_name='Simulation End',
+    #                             help_text='The end date of the model simulation (mm/dd/yyyy hh:mm)')
+    # hm_timestep = models.FloatField(verbose_name='Simulation Time Interval',
+    #                              help_text='The timestep interval that is used for calculations (in seconds)')
+
 
 
 
